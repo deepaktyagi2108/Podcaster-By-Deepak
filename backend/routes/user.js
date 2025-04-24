98c3168cd229d355a3312548f42bcb6f8717e394
DEEPAK.TYAGI <tyagideepak607@gmail.com>
2025-04-24 08:26:16 +0530

    edit: reduce JWT token expiration and cookie max age to 1 day

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -33,14 +33,14 @@ router.post("/sign-up", async (req, res) => {
 
     console.log('JWT_SECRET:', process.env.JWT_SECRET);
     const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-      expiresIn: "7d",
+      expiresIn: "1d",
     });
 
     res.cookie("token", token, {
       httpOnly: true,
       secure: false, // set to true in production (https)
       sameSite: "lax",
-      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+      maxAge: 1 * 24 * 60 * 60 * 1000, // 1
     });
 
     return res.status(201).json({ user: newUser });
