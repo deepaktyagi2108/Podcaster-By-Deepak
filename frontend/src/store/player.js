053a7ca259efe53851001f35f0b3725b35deff4b
DEEPAK.TYAGI <tyagideepak607@gmail.com>
2025-05-29 23:03:33 +0530

    player fixed

--- frontend/src/store/player.js
+++ frontend/src/store/player.js
@@ -1,5 +1,34 @@
 
 
+// import { createSlice } from "@reduxjs/toolkit";
+
+// const playerSlice = createSlice({
+//   name: "player",
+//   initialState: {
+//     songPath: "",
+//     img: "",
+//     isPlayerDiv: false,
+//   },
+//   reducers: {
+//     changeSong: (state, action) => {
+//       state.songPath = action.payload;
+//     },
+//     changeImage: (state, action) => {
+//       state.img = action.payload;
+//     },
+//     setDiv: (state) => {
+//       state.isPlayerDiv = true;
+//     },
+//     closeDiv: (state) => {
+//       state.songPath = "";
+//       state.img = "";
+//       state.isPlayerDiv = false;
+//     },
+//   },
+// });
+
+// export const playerActions = playerSlice.actions;
+// export default playerSlice.reducer;
 import { createSlice } from "@reduxjs/toolkit";
 
 const playerSlice = createSlice({
@@ -8,6 +37,7 @@ const playerSlice = createSlice({
     songPath: "",
     img: "",
     isPlayerDiv: false,
+    isPlaying: false, // <-- add this
   },
   reducers: {
     changeSong: (state, action) => {
@@ -23,10 +53,18 @@ const playerSlice = createSlice({
       state.songPath = "";
       state.img = "";
       state.isPlayerDiv = false;
+      state.isPlaying = false;
     },
+    startPlaying: (state) => {
+      state.isPlaying = true;
+    }, // <-- add this
+    pausePlaying: (state) => {
+      state.isPlaying = false;
+    }, // <-- optional: add this too
   },
 });
 
 export const playerActions = playerSlice.actions;
 export default playerSlice.reducer;
+
 
